bb3620b8eb36e7f3e3a327ae2dfbffab5b076d3e
Rolex <rolex@Rolexs-MacBook-Pro.local>
2021-01-21 17:26:35 -0800

    update ccache & dialog

--- ccache.mk
+++ ccache.mk
@@ -8,14 +8,14 @@ DEB_CCACHE_V   ?= $(CCACHE_VERSION)
 
 ccache-setup: setup
         wget -q -nc -P $(BUILD_SOURCE) https://github.com/ccache/ccache/releases/download/v$(CCACHE_VERSION)/ccache-$(CCACHE_VERSION).tar.xz
-        $(call EXTRACT_TAR,ccache-$(CCACHE_VERSION).tar.xz,ccache-$(CCACHE_VERSION),ccache)
+	$(call EXTRACT_TAR,ccache-$(CCACHE_VERSION).tar.xz,ccache-$(CCACHE_VERSION),ccache)
 
 ifneq ($(wildcard $(BUILD_WORK)/ccache/.build_complete),)
 ccache:
         @echo "Using previously built ccache."
 else
 ccache: ccache-setup
-        cd $(BUILD_WORK)/ccache && cmake . -j$(shell $(GET_LOGICAL_CORES)) \
+	cd $(BUILD_WORK)/ccache && cmake . -j$(shell $(GET_LOGICAL_CORES)) \
                 -DCMAKE_BUILD_TYPE=Release \
                 -DCMAKE_SYSTEM_NAME=Darwin \
                 -DCMAKE_CROSSCOMPILING=true \
@@ -35,27 +35,27 @@ ccache: ccache-setup
                 -DCOMMON_ARCH=$(DEB_ARCH) \
                 -DCMAKE_FIND_ROOT_PATH=$(BUILD_BASE) \
                 .
-        +$(MAKE) -C $(BUILD_WORK)/ccache
-        +$(MAKE) -C $(BUILD_WORK)/ccache install \
+	+$(MAKE) -C $(BUILD_WORK)/ccache
+	+$(MAKE) -C $(BUILD_WORK)/ccache install \
                 DESTDIR="$(BUILD_STAGE)/ccache"
-        touch $(BUILD_WORK)/ccache/.build_complete
+	touch $(BUILD_WORK)/ccache/.build_complete
 endif
 
 ccache-package: ccache-stage
         # ccache.mk Package Structure
-        rm -rf $(BUILD_DIST)/ccache
-        mkdir -p $(BUILD_DIST)/ccache
+	rm -rf $(BUILD_DIST)/ccache
+	mkdir -p $(BUILD_DIST)/ccache
 
         # ccache.mk Prep ccache
-        cp -a $(BUILD_STAGE)/ccache/usr $(BUILD_DIST)/ccache
+	cp -a $(BUILD_STAGE)/ccache/usr $(BUILD_DIST)/ccache
 
         # ccache.mk Sign
-        $(call SIGN,ccache,general.xml)
+	$(call SIGN,ccache,general.xml)
 
         # ccache.mk Make .debs
-        $(call PACK,ccache,DEB_CCACHE_V)
+	$(call PACK,ccache,DEB_CCACHE_V)
 
         # ccache.mk Build cleanup
-        rm -rf $(BUILD_DIST)/ccache
+	rm -rf $(BUILD_DIST)/ccache
 
 .PHONY: ccache ccache-package
